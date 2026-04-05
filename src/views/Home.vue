a16db3243b3693733f5d01f288f8b7880efc3342
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2026-04-05 12:42:08 +0600

    Add Add.vue and Update.vue pages with routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="home">
-    <h1>Welcome to the Home Page!</h1>
+    <h1 v-if="name">Welcome {{ name }} to the Home Page!</h1>
+    <h1 v-else>Welcome to the Home Page!</h1>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const name = ref('')
+
+onMounted(() => {
+  
+  const user = JSON.parse(localStorage.getItem('users'))
+  if (user && user.name) {
+    name.value = user.name
+  }
+})
 </script>
+
 
 <style scoped>
 .home {
